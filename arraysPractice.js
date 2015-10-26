61db2c9fbc5cad61f30a79ecb4c0042b0e7b3ff9
Brendan Doyle <bdoyle522@gmail.com>
2015-10-26 14:23:14 -0500

    forgot one, now complete

--- arraysPractice.js
+++ arraysPractice.js
@@ -233,8 +233,21 @@ function here (arr1,arr2){
   'both' should return a new array full of numbers that are found in both arr1 and arr2.
 */
 
-function here (arr1,arr2){
-  
+function both (arr1,arr2){
+  var ret = [];
+  if(arr1.length>arr2.length){
+    for(var i = 0; i < arr1.length; i++){
+      if(arr2.indexOf(arr1[i]) !== -1){
+        ret.push(arr1[i]);
+      }
+    }
+  }
+  else{
+    for(var i = 0; i < arr2.length; i++){
+      if(arr1.indexOf(arr2[i]) !== -1){
+        ret.push(arr2[i]);
+  }
+  return ret;
 }
 //NEXT PROBLEM
 
